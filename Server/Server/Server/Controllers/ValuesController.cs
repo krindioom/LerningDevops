8e2a0d9d9e5f4273e739ebe166850973591fec2d
krindioom <47291437+krindioom@users.noreply.github.com>
2024-10-05 15:07:15 +0700

    создан тестовый контроллер

---
+++ Server/Server/Server/Controllers/ValuesController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Server.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        [HttpGet("values")]
+        public async Task<ActionResult> GetValuesAsync()
+        {
+            var values = new 
+            { 
+                Value1 = 2,
+                Value2 = 3,
+                Value3 = 4,
+                Value4 = 5,
+                Value5 = 6,
+                Value6 = 7,
+                Value7 = 8,
+                Value8 = 9,
+            };
+            return Ok(values);
+        }
+    }
+}
